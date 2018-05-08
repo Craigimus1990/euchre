d92c0b07b7ef4a200bcc1a0040a1fcc56f6907d6
Max <mxwllhllm@gmail.com>
2018-05-07 21:50:02 -0400

    commit

--- rules_engine.py
+++ rules_engine.py
@@ -1,22 +1,43 @@
 from deck import Deck, Card
 import random
 
+class EuchrePlayer:
+
+  def __init__(self, team):
+    self.team = team
+
+  def dealHand(self, hand):
+    self.hand = hand
+
+  def playCard(self, pot, trump, handScores, teamScores):
+    return self.hand.pop(0)
+
+  def pickKitty(self, kitty):
+    return True
+
+  def pickTrump(self, invalid, forced=False):
+    return self.hand[0].suit
+
+  def exchangeCard(self, card):
+    ret, self.hand[0] = self.hand[0], card
+    return ret
+
 class Euchre:
 
-  def __init__(self):
+  def __init__(self, player1, player2, player3, player4):
     ## Setup game
     self.deck = Deck()
     random.seed()
 
     # Setup team 1
     self.team1Score = 0
-    playerA_1 = []
-    playerB_1 = []
+    self.playerA_1 = player1
+    self.playerB_1 = player2
     
     # Setup team 2
     self.team2Score = 0
-    playerA_2 = []
-    playerB_2 = []
+    self.playerA_2 = player3
+    self.playerB_2 = player4
     
     # Setup order
     self.turn_order = [playerA_1, playerA_2, playerB_1, playerB_2]
@@ -32,12 +53,39 @@ class Euchre:
     self.deck.shuffle()
     
     for player in self.turn_order:
-      player.extend(self.deck.deal(5))
+      (self.deck.deal(5))
     
-    kitty = self.deck.deal(1)
-    print("Kitty: " + kitty[0].__str__())
+    kitty = self.deck.deal(1)[0]
+    picking = False
+    print("Kitty: " + kitty.__str__())
+
+    for player in self.turn_order:
+      picking = player.pickKitty(kitty)
+
+      if (picking):
+        break
+
     
-  def playPhase(self):
+    if (picking):
+      exchange_check = self.turn_order[-1].exchangeCard(kitty)
+      if (exchange_check == kitty):
+        self.add_score(self.turn_order[0], 2)
+        return -1
+      return kitty.suit
+
+    for player in self.turn_order:
+      forced = (player == self.turn_order[-1])
+      suit = player.pickTrump(kitty.suit, forced)
+      if (suit == kitty.suit or (suit is None and forced == True)):
+        renegedPlayer = self.turn_order.index(player)
+        self.add_score(self.turn_order[(renegedPlayer + 1) % 4], 2)
+        return -1
+      
+      if (suit is not None):
+        return suit
+      
+
+  def playPhase(self, trump):
     for player in self.turn_order:
       for card in player:
         print(card.__str__(), end=' ')
@@ -46,8 +94,11 @@ class Euchre:
   def play(self):
     while (self.team1Score < 10 and self.team2Score < 10):
       self.team1Score = 10
-      self.dealPhase()
-      self.playPhase()  
+      suit = self.dealPhase()
+      if (suit == -1):
+        continue
+
+      self.playPhase(trump=suit)  
 
 
 game = Euchre()
